01e27a73c8051b63205fe85ba902235aa6dc09fe
wensen2525 <109505910+wensen2525@users.noreply.github.com>
2023-03-02 01:56:23 +0700

    123

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
                                     </div>
                               @endif
                               <a href="" target=""><i class="bi bi-arrow-left btn btn-primary px-2 py-1 text-light position-absolute start-0" style="font-size:13px;"></i></a>
-                              <div class="border border-1 border-dark rounded-3 p-0">
+                              <div class="border border-1 border-dark rounded-3 p-0 my-2">
                                     <div class="border-dark row col-12 m-auto">
                                           <div class="col-12 col-lg-6 border-bottom-lg border-dark d-flex justify-content-center p-3">
                                                 <p class="fs-4 m-0"><strong>E-CERTIFICATE</strong></p>
@@ -27,30 +27,26 @@
                                     </div>
                                     <div class="border-bottom border-top border-dark d-flex justify-content-center p-0">
                                           <form action="" method="get">
-                                                <div class="d-flex gap-5 align-items-center">
-                                                      <div class="" style="opacity: 0.5">
+                                                <div class="d-flex gap-5 align-items-center py-3">
+                                                      {{-- <div class="" style="opacity: 0.5">
                                                             <th scope="col"><strong>Created at:</strong> 2023-11-02 11:22:22 AM</th>
-                                                      </div>
+                                                      </div> --}}
 
                                                       <a class="btn btn-danger px-3 py-1" href="{{ route('participants.delete-all') }}">Delete</a>
-                                                      {{-- <a class="btn btn-primary px-3 py-1" href="{{ route('participants.update-all',['value' => 'All']) }}">All</a>
-                                                      <a class="btn btn-primary px-3 py-1" href="{{ route('participants.update-all',['value' => 'User']) }}">User</a>
-                                                      <a class="btn btn-primary px-3 py-1" href="{{ route('participants.update-all',['value' => 'Guest']) }}">Guest</a> --}}
-                                                      {{-- <a class="btn btn-primary px-3 py-1" href="{{ route('participants.update',$participants) }}">User</a>
-                                                      <a class="btn btn-primary px-3 py-1" href="{{ route('participants.update',$participants) }}">Guest</a> --}}
-                                                      <div class="" style="opacity: 0.5">
+
+                                                      {{-- <div class="" style="opacity: 0.5">
                                                             <th scope="col"><strong>Updated at:</strong> 2023-11-02 11:22:22 AM</th>
-                                                      </div>
+                                                      </div> --}}
                                                 </div>
                                           </form>
                                     </div>
                                     <div class="border-bottom border-dark d-flex justify-content-center p-3 gap-3">
                                           <p class="fs-5 m-0">DATA - 1</p>
                                           <a href="{{route('participants.export')}}"><i class="btn btn-primary px-2 py-1 text-light" style="font-size:13px;">Export Data</i></a>
-                                          <a href="{{route('ceritificates.viewpdf')}}" target="_blank"><i class="bi bi-eye btn btn-primary px-2 py-1 text-light" style="font-size:13px;"></i></a>
-                                          <a href="{{route('ceritificates.download-all-certificates','1 - 50')}}"><i class="bi bi-download btn btn-primary px-2 py-1 text-light" style="font-size:13px;">  1 - 50</i></a>
-                                          <a href="{{route('ceritificates.download-all-certificates','51 - 100')}}"><i class="bi bi-download btn btn-primary px-2 py-1 text-light" style="font-size:13px;">  51 - 100</i></a>
-                                          <a href="{{route('ceritificates.send-all-certificates','1 - 2') }}"><i class="bi bi-send btn btn-danger px-2 py-1 text-light" style="font-size:13px;">  1 - 2</i></a>
+                                          {{-- <a href="{{route('ceritificates.viewpdf')}}" target="_blank"><i class="bi bi-eye btn btn-primary px-2 py-1 text-light" style="font-size:13px;"></i></a> --}}
+                                          <a href="{{route('ceritificates.download-all-certificates','1 - 50')}}"><i class="bi bi-cloud-arrow-up-fill btn btn-primary px-2 py-1 text-light" style="font-size:13px;">  1 - 50</i></a>
+                                          <a href="{{route('ceritificates.download-all-certificates','51 - 100')}}"><i class="bi bi-cloud-arrow-up-fill btn btn-primary px-2 py-1 text-light" style="font-size:13px;">  51 - 100</i></a>
+                                          {{-- <a href="{{route('ceritificates.send-all-certificates','1 - 2') }}"><i class="bi bi-send btn btn-danger px-2 py-1 text-light" style="font-size:13px;">  1 - 2</i></a> --}}
 
 
                                     </div>
